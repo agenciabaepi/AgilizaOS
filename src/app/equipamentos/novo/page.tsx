4c7692e5c05cb456534399358b0a90c0ac0c69e4
Lucas Oliveira <agenciabaepi@gmail.com>
2025-07-19 23:54:59 -0300

    feat:tentar  criar página de visualização de cliente

--- src/app/equipamentos/novo/page.tsx
+++ src/app/equipamentos/novo/page.tsx
@@ -1,6 +1,6 @@
-'use client';
-
 export const dynamic = 'force-dynamic';
+export const runtime = 'edge';
+'use client';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { useState, useEffect } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
