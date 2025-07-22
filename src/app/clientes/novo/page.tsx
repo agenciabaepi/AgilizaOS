a41316df972d650b7eeae7916e5050a57d0de47f
Lucas Oliveira <agenciabaepi@gmail.com>
2025-07-22 16:29:50 -0300

    feat:tentar  criar página de visualização de cliente

--- src/app/clientes/novo/page.tsx
+++ src/app/clientes/novo/page.tsx
@@ -4,8 +4,17 @@ import { useSearchParams } from 'next/navigation';
 import ClienteForm from '../../../components/ClienteForm';
 import ProtectedArea from '@/components/ProtectedArea';
 import MenuLayout from '@/components/MenuLayout';
+import { Suspense } from 'react';
 
 export default function NovoClientePage() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <NovoClientePageInner />
+    </Suspense>
+  );
+}
+
+function NovoClientePageInner() {
   const searchParams = useSearchParams();
   const returnToOS = searchParams.get('returnToOS') === 'true';
   return (
